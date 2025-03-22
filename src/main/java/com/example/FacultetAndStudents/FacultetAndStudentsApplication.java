1487b7d0393aac62c34ca74bb4bc950dc79be16d
Евгений <Safronovbr@mail.ru>
2025-03-22 18:25:20 +0300

    Started

---
+++ src/main/java/com/example/FacultetAndStudents/FacultetAndStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FacultetAndStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultetAndStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultetAndStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FacultetAndStudents/FacultetAndStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FacultetAndStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultetAndStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
